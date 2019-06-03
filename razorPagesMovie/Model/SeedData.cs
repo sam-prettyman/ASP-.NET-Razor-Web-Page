55bfe473dfedd6a572c7a66d2638ce9f95e2dc50
sam-prettyman <35538856+sam-prettyman@users.noreply.github.com>
2019-06-03 17:22:29 -0600

    Add project files.

---
+++ razorPagesMovie/Model/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using razorPagesMovie.Models;
+namespace razorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new razorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<razorPagesMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+               
+
+                context.Movie.AddRange(
+                    new RazorPagesMovie.Models.Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                         Rating = "R"
+                    },
+
+                    new RazorPagesMovie.Models.Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                         Rating = "R"
+                    },
+
+                    new RazorPagesMovie.Models.Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+
+                    new RazorPagesMovie.Models.Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ razorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Models;
+using razorPagesMovie.Models;
+
+namespace razorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly razorPagesMovie.Models.razorPagesMovieContext _context;
+
+        public CreateModel(razorPagesMovie.Models.razorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
